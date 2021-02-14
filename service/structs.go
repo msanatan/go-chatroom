2363f9537f5416435e4ca07fb85737db143c16e7
Marcus Sanatan <msanatan@gmail.com>
2021-02-13 23:18:44 -0400

    Add a Bot interface
    An interface allows us to mock a bot and work on its integration with chat.
    Its intentionally simple, gets a string and returns a string/error.
    May refactor to return a message

--- service/structs.go
+++ service/structs.go
@@ -3,4 +3,10 @@ package service
 // MessagePayload is the envelope for messages sent to and from the chat participants
 type MessagePayload struct {
 	Message string `json:"message"`
+	Type    string `json:"type"`
+}
+
+// Bot is an interface for chatbots
+type Bot interface {
+	ProcessCommand(arguments string) (string, error)
 }
