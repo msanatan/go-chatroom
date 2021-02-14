6ffbd0859301c6b6491ba1343f4b9ae06999cef1
Marcus Sanatan <msanatan@gmail.com>
2021-02-14 17:57:51 -0400

    Generalize which queues RabbitMQ client can work with
    This is a nice/cheap way to reuse the client on the bot side

--- app/rabbitmq/client.go
+++ app/rabbitmq/client.go
@@ -9,73 +9,71 @@ import (
 
 // Client publishes and consumes messages on RabbitMQ queues
 type Client struct {
-	Channel        *amqp.Channel
-	commandRequest amqp.Queue
-	botResponse    amqp.Queue
-	logger         *log.Entry
+	Channel           *amqp.Channel
+	requestQueueName  string
+	responseQueueName string
+	requestQueue      amqp.Queue
+	responseQueue     amqp.Queue
+	logger            *log.Entry
 }
 
 // NewClient instantiates a new Rabbit MQ client
-func NewClient(channel *amqp.Channel, logger *log.Entry) *Client {
+func NewClient(channel *amqp.Channel, requestQueue, responseQueue string, logger *log.Entry) *Client {
 	return &Client{
-		Channel: channel,
-		logger:  logger,
+		Channel:           channel,
+		requestQueueName:  requestQueue,
+		responseQueueName: responseQueue,
+		logger:            logger,
 	}
 }
 
 // QueueDeclare declares the queues so we can publish and consume messages
 func (c *Client) QueueDeclare() error {
 	logger := c.logger.WithField("method", "QueueDeclare")
-	commandRequest, err := c.Channel.QueueDeclare(
-		"command_queue", // name
-		false,           // durable
-		false,           // delete when unused
-		false,           // exclusive
-		false,           // no-wait
-		nil,             // arguments
+	requestQueue, err := c.Channel.QueueDeclare(
+		c.requestQueueName, // name
+		false,              // durable
+		false,              // delete when unused
+		false,              // exclusive
+		false,              // no-wait
+		nil,                // arguments
 	)
 	if err != nil {
 		logger.Errorf("could not create command queue to send messages: %s", err.Error())
 		return err
 	}
 
-	c.commandRequest = commandRequest
+	c.requestQueue = requestQueue
 
-	botResponse, err := c.Channel.QueueDeclare(
-		"response_queue", // name
-		false,            // durable
-		false,            // delete when unused
-		false,            // exclusive
-		false,            // no-wait
-		nil,              // arguments
+	responseQueue, err := c.Channel.QueueDeclare(
+		c.responseQueueName, // name
+		false,               // durable
+		false,               // delete when unused
+		false,               // exclusive
+		false,               // no-wait
+		nil,                 // arguments
 	)
 	if err != nil {
 		logger.Errorf("could not create response queue to receive messages: %s", err.Error())
 		return err
 	}
 
-	c.botResponse = botResponse
+	c.responseQueue = responseQueue
 	logger.Debug("declared command_queue and response_queue queues")
 	return nil
 }
 
 // Publish sends a message to the command queue
-func (c *Client) Publish(payload BotMessagePayload) error {
-	logger := c.logger.WithField("method", "Publish")
-	payloadBytes, err := json.Marshal(payload)
-	if err != nil {
-		logger.Errorf("could not marshal payload to publish request: %s", err.Error())
-	}
-
+func (c *Client) Publish(payload json.RawMessage) error {
 	return c.Channel.Publish(
-		"",                    // exchange
-		c.commandRequest.Name, // routing key
-		false,                 // mandatory
-		false,                 // immediate
+		"",                  // exchange
+		c.requestQueue.Name, // routing key
+		false,               // mandatory
+		false,               // immediate
 		amqp.Publishing{
 			ContentType: "text/plain",
-			Body:        payloadBytes,
-			ReplyTo:     c.commandRequest.Name,
+			Body:        payload,
+			ReplyTo:     c.requestQueue.Name,
 		},
 	)
 }
@@ -83,12 +81,12 @@ func (c *Client) Publish(payload BotMessagePayload) error {
 // Consume receives a message from the response queue
 func (c *Client) Consume() (<-chan amqp.Delivery, error) {
 	return c.Channel.Consume(
-		c.botResponse.Name, // queue
-		"",                 // consumer
-		true,               // auto-ack
-		false,              // exclusive
-		false,              // no-local
-		false,              // no-wait
-		nil,                // args
+		c.responseQueue.Name, // queue
+		"",                   // consumer
+		true,                 // auto-ack
+		false,                // exclusive
+		false,                // no-local
+		false,                // no-wait
+		nil,                  // args
 	)
 }
