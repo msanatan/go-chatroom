3ae2e6661aea12f9c256b42035ac61e10049d870
Marcus Sanatan <msanatan@gmail.com>
2021-02-14 14:45:32 -0400

    Initialize app so it runs with RabbitMQ

--- app/main.go
+++ app/main.go
@@ -7,15 +7,46 @@ import (
 
 	"github.com/gorilla/mux"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/msanatan/go-chatroom/app/rabbitmq"
 	"github.com/msanatan/go-chatroom/app/service"
 	"github.com/msanatan/go-chatroom/utils"
+	"github.com/streadway/amqp"
 )
 
 func main() {
 	logLevel := os.Getenv("LOG_LEVEL")
 	logger := utils.InitLogger(logLevel, "chatroom")
-	wsServer := service.NewServer(nil, "/", logger)
+	var wsServer *service.Server
+	var rabbitMQClient *rabbitmq.Client
+
+	rabbitConnection := os.Getenv("RABBITMQ_CONNECTION")
+	if rabbitConnection == "" {
+		logger.Error("No RabbitMQ connection string provided, will not setup connection")
+	} else {
+		conn, err := amqp.Dial(rabbitConnection)
+		if err != nil {
+			logger.Fatalf("could not connect to RabbitMQ instance: %s", err.Error())
+		}
+		defer conn.Close()
+
+		ch, err := conn.Channel()
+		if err != nil {
+			logger.Fatalf("could not open a channel: %s", err.Error())
+		}
+		defer ch.Close()
+
+		rabbitMQClient = rabbitmq.NewClient(ch, logger)
+		err = rabbitMQClient.QueueDeclare()
+		if err != nil {
+			logger.Fatalf("could not declare queues: %s", err.Error())
+		}
+	}
+
+	wsServer = service.NewServer(rabbitMQClient, nil, "/", logger)
 	go wsServer.Run()
+	if rabbitMQClient != nil {
+		go wsServer.ConsumeRMQ()
+	}
 
 	// Setup default WS Client config
 	defaultClientConfig := &service.ClientConfig{
