cc1a69cf8943dda15c3d8bd3467b95f03699260f
Marcus Sanatan <msanatan@gmail.com>
2021-02-15 23:07:01 -0400

    Update corresponding struct in general server comms
    These fields are used right now with the websocket, but we can get them.

--- app/service/structs.go
+++ app/service/structs.go
@@ -2,6 +2,8 @@ package service
 
 // MessagePayload is the envelope for messages sent to and from the chat participants
 type MessagePayload struct {
-	Message string `json:"message"`
-	Type    string `json:"type"`
+	Message  string `json:"message"`
+	Type     string `json:"type"`
+	Username string `json:"username"`
+	Created  string `json:"created"`
 }
